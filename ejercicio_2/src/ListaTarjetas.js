e91702b96b257fd7cee14efea2045f0dd38f0a6c
AlumnoL13 <AlumnoL13@teccan.edu.mx>
2023-03-28 18:59:21 -0700

    Cambios realizados

--- ejercicio_2/src/ListaTarjetas.js
+++ ejercicio_2/src/ListaTarjetas.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import Encabezado from './Encabezado'
 import Tarjeta from './Tarjeta'
 
 export default function ListaTarjetas() {
   return (
     <div className='Container'>
-        <div className='row'>
-            <div className="col-4"><Tarjeta/></div>
-            <div className="col-4"><Tarjeta/></div>
-            <div className="col-4"><Tarjeta/></div>
-        </div>
+      <div className='row'>
+        <Encabezado titulo="Tarjetas"/>
+      </div>
+      <div className='row'>
+          <div className="col-4"><Tarjeta datos={{titulo:"Nota_1",texto:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",urlImagen:"https://picsum.photos/120/120?random=1",botontxt:"Seguir leyendo"}}/></div>
+          <div className="col-4"><Tarjeta datos={{titulo:"Nota_2",texto:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",urlImagen:"https://picsum.photos/120/120?random=2",botontxt:"Seguir leyendo"}}/></div>
+          <div className="col-4"><Tarjeta datos={{titulo:"Nota_3",texto:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",urlImagen:"https://picsum.photos/120/120?random=3",botontxt:"Seguir leyendo"}}/></div>
+       </div>
     </div>
   )
 }
--- ejercicio_2/src/Tarjeta.js
+++ ejercicio_2/src/Tarjeta.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-export default function 
-() {
+export default function Tarjeta ({datos}) {
   return (
       //En lugar de class se coloca className ademas de que los estilos en lugar de tener comillas se colocaran doble llave y el valor de la propiedad 
       //debera ir entre comillas.
     <div className="card" style={{width: "18rem"}}>
-    <img src="https://picsum.photos/120/120?random=1" className="card-img-top" alt="..."/>
+    <img src={datos.urlImagen} className="card-img-top" alt="..."/>
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
+      <h5 className="card-title">{datos.titulo}</h5>
+      <p className="card-text">{datos.texto}</p>
+      <a href="#" className="btn btn-primary">{datos.botontxt}</a>
     </div>
    </div>
   )
