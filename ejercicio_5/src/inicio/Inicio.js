90d157d2ffece75a41514997bb4d5463d90487d3
AlumnoL13 <AlumnoL13@teccan.edu.mx>
2023-04-20 19:12:35 -0700

    Avance ejericio 5

--- ejercicio_5/src/inicio/Inicio.js
+++ ejercicio_5/src/inicio/Inicio.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Inicio() {
+  
+  //Defincion de estado para ID
+  const [id, setId] = useState(0);
+  //
+
   return (
-    <div>Inicio</div>
+    <>
+    <div class="container">
+    <form>
+      <div class="mb-4">
+        <label for="ID_B" class="form-label">id a Buscar</label>
+        <input value={id} type="text" class="form-control" id="ID_B" onChange={(e) => setId(id => e.target.value)}/>
+      </div>
+      <a href={'/datos/'+id} class='btn btn-primary'>Buscar</a>
+      {/* <a href={`/datos/ $id`} class='btn btn-primary'>Buscar</a> */}
+    </form>
+    </div>  
+    </>
   )
 }
